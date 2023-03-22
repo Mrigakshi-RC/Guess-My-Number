6bb666ded3bd5c199aa0cc69a73eaa76233a3c28
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-03-22 18:32:11 +0530

    worked on the logic

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+let guess=document.querySelector(".guess");
+let message=document.querySelector(".message");
+const check_btn=document.querySelector(".check");
+const body=document.querySelector("body");
+
+function check(number){
+    if (number>15){
+        message.innerText="Too High";
+    }
+    else if (number<15){
+        message.innerText="Too Low";
+    }
+    else{
+        message.innerText="Correct Number!";
+        body.style.backgroundColor="#60b347";
+    }
+}
+
+check_btn.addEventListener("click", function() {
+    check(Number(guess.value));
+})
