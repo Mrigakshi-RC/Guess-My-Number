a932d80f6cc66094d8e9584e9180b692eed878d5
Mrigakshi Roy Choudhury <mrigakshirc@gmail.com>
2023-03-31 12:39:25 +0530

    dealt with score display and restore function

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 'use strict';
 let guess=document.querySelector(".guess");
+let initialGuess=guess;
+
 let message=document.querySelector(".message");
+let initialMessage=message;
+
 const check_btn=document.querySelector(".check");
+let initialCheck=check_btn;
+
 const answer=document.querySelector(".number");
 const body=document.querySelector("body");
-let score = Math.floor((Math.random() * 20) + 1);
+let initialBody=body;
+
+let again_btn=document.querySelector(".again");
+
+let correct = Math.floor((Math.random() * 20) + 1);
+let score=20;
+
 
 function check(number){
-    if (number>score){
-        message.innerText="📈 Too High";
-    }
-    else if (number<score){
-        message.innerText="📉 Too Low";
-    }
-    else{
+    if (number===correct){
         message.innerText="Correct Number!";
         body.style.backgroundColor="#60b347";
-        answer.innerText=score;
+        answer.innerText=correct;
     }
+    else if (score>1){
+        if (number>correct){
+            message.innerText="📈 Too High";
+            score--;
+            document.querySelector(".score").textContent=score;
+        }
+        else if (number<correct){
+            message.innerText="📉 Too Low";
+            score--;
+            document.querySelector(".score").textContent=score;
+        }
+    }
+    else{
+        score=0;
+        document.querySelector(".score").textContent=score;
+        message.innerText="You lost the game 💥";
+    }
+
 }
+
+function restore(){};
 
 check_btn.addEventListener("click", function() {
     check(Number(guess.value));
